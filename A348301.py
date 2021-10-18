f89b3794d849c8a02aae3b214aabe078c3a51270
Greg Tener <borisb0ri5@gmail.com>
2021-10-18 00:42:47 -0700

    Update A348301.py

--- A348301.py
+++ A348301.py
@@ -1,12 +1,9 @@
-import sympy
-
 from itertools import islice
 from sympy import primorial, sieve
 
 
 def numerator(n):
-    first_n_primes = list(islice(sieve, n))
-    return sum((primorial(n) // first_n_primes[i] for i in range(n)))
+    return sum((primorial(n) // p for p in islice(sieve, n)))
 
 
 def sequence(n):
