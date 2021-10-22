21e9c9930509a7f307c16438743f3a8ea1fb9b26
Greg Tener <borisb0ri5@gmail.com>
2021-10-22 08:03:19 -0700

    .

--- A059861.py
+++ A059861.py
@@ -6,3 +6,8 @@ def brute_force(n):
     return sum(
         gcd(r, primorial(n)) == 1 and gcd(r + 2, primorial(n)) == 1
         for r in range(0, primorial(n)))
+
+
+def r_times_r_plus_two(n):
+    return sum(
+        gcd(r * (r + 2), primorial(n)) == 1 for r in range(0, primorial(n)))
--- A059861_test.py
+++ A059861_test.py
@@ -14,6 +14,14 @@ class Test(unittest.TestCase):
         self.assertEqual(135, brute_force(5))
         self.assertEqual(1485, brute_force(6))
 
+    def test_brute_force(self):
+        self.assertEqual(1, r_times_r_plus_two(1))
+        self.assertEqual(1, r_times_r_plus_two(2))
+        self.assertEqual(3, r_times_r_plus_two(3))
+        self.assertEqual(15, r_times_r_plus_two(4))
+        self.assertEqual(135, r_times_r_plus_two(5))
+        self.assertEqual(1485, r_times_r_plus_two(6))
+
 
 if __name__ == '__main__':
     unittest.main()
